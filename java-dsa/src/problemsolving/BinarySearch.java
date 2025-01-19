3c0ecb0df2f689e087aa58760ab42f1d5d526664
Shreeraam G <shreeram0312@gmail.com>
2025-01-19 22:22:26 +0530

    Binary Search for multiple occurences

---
+++ java-dsa/src/problemsolving/BinarySearch.java
@@ -0,0 +1,34 @@
+package problemsolving;
+
+// Binary Search for multiple occurences
+// https://www.geeksforgeeks.org/problems/binary-search-1587115620/1
+
+public class BinarySearch {
+  public static void main(String[] args) {
+    int[] nums = { 2, 2, 2, 2, 2, 4, 4, 5, 6, 6, 7, 8 };
+    int target = 2;
+
+    int index = binarySearch(nums, target);
+    System.out.println(index);
+  }
+
+  public static int binarySearch(int[] nums, int target) {
+    int s = 0, e = nums.length - 1;
+    int ans = -1;
+
+    while (s <= e) {
+      int mid = s + (e - s) / 2;
+
+      if (nums[mid] == target) {
+        ans = mid;
+      }
+      if (nums[mid] >= target) {
+        e = mid - 1;
+      } else {
+        s = mid + 1;
+      }
+    }
+
+    return ans;
+  }
+}
