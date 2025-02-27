140b1bf0305cf893982be8b084bd946aba0d3395
Shreeraam G <shreeram0312@gmail.com>
2025-02-27 22:13:36 +0530

    ATOI - ASCII to Integer

--- java-dsa/src/problemsolving/Atoi.java
+++ java-dsa/src/problemsolving/Atoi.java
@@ -1,9 +1,16 @@
 package problemsolving;
 
+// ATOI - ASCII to Integer
+// https://www.geeksforgeeks.org/write-your-own-atoi/
+
 class Atoi {
   public static void main(String[] args) {
     int res1 = atoi(" -123123");
     System.out.println(res1);
+    int res2 = atoi(" -0012gfg4");
+    System.out.println(res2);
+    int res3 = atoi("  1231231231311133");
+    System.out.println(res3);
   }
 
   static int atoi(String s) {
@@ -19,7 +26,7 @@ class Atoi {
       index++;
     }
 
-    while (index < s.length() && s.charAt(index) > '0' && s.charAt(index) < '9') {
+    while (index < s.length() && s.charAt(index) >= '0' && s.charAt(index) <= '9') {
       if (result > Integer.MAX_VALUE / 10 || result == Integer.MAX_VALUE / 10 && s.charAt(index) - '0' > 7)
         return sign == 1 ? Integer.MAX_VALUE : Integer.MIN_VALUE;
 
