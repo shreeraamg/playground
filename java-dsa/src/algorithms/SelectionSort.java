a5400342d4e86c0ba267d0254891ff4091d00169
Shreeraam G <shreeram0312@gmail.com>
2025-07-19 13:41:56 +0530

    Selection Sort

---
+++ java-dsa/src/algorithms/SelectionSort.java
@@ -0,0 +1,24 @@
+package algorithms;
+
+import java.util.Arrays;
+
+public class SelectionSort {
+    public static void main(String[] args) {
+        int[] arr = {13, 46, 24, 52, 20, 9};
+        System.out.println(Arrays.toString(arr));
+
+        for (int i=0; i<arr.length-1; i++) {
+            int min = i;
+            for (int j=i+1; j<arr.length; j++) {
+                if (arr[j] < arr[min]) {
+                    min = j;
+                }
+            }
+            int temp = arr[min];
+            arr[min] = arr[i];
+            arr[i] = temp;
+        }
+
+        System.out.println(Arrays.toString(arr));
+    }
+}
---
+++ python/algorithms/selection_sort.py
@@ -0,0 +1,11 @@
+arr = [13, 46, 24, 52, 20, 9]
+print(arr)
+
+for i in range(0, len(arr) - 1):
+    minimum = i
+    for j in range(i+1, len(arr)):
+        if arr[j] < arr[minimum]:
+            minimum = j
+    arr[i], arr[minimum] = arr[minimum], arr[i]
+
+print(arr)
