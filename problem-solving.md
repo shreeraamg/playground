0f3609e700d5694b4a9fa2a964e30be198849930
Shreeraam G <shreeram0312@gmail.com>
2025-07-24 09:29:25 +0530

    Reverse Array

--- problem-solving.md
+++ problem-solving.md
@@ -2,3 +2,4 @@
 
 1. [Merge Two Sorted Array (with extra space)](python/problemsolving/merge_sroted_array.py)
 2. [Merge Sorted Array (in place)](python/problemsolving/leetcode_88.py)
+3. [Reverse an Array](python/problemsolving/reverse_array.py)
---
+++ python/problemsolving/reverse_array.py
@@ -0,0 +1,19 @@
+"""
+Reverse an Array
+https://www.geeksforgeeks.org/dsa/program-to-reverse-an-array
+"""
+
+
+def reverse(arr):
+    i, j = 0, len(arr) - 1
+
+    while i < j:
+        arr[i], arr[j] = arr[j], arr[i]
+        i += 1
+        j -= 1
+
+
+arr1 = [1, 5, 3, 8, 9, 10, 7]
+print("Original:", arr1)
+reverse(arr1)
+print("Reversed:", arr1)
